616ca6af5c2ad5ed527b2618c9bea90b138f2d42
Ryan Van Etten <ryanve@gmail.com>
2018-01-06 11:23:57 -0800

    Use double quotes

--- said.js
+++ said.js
@@ -2,8 +2,8 @@ function trim(x) {
   return String(x).trim()
 }
 
-var fm = require('fm')
-var cp = require('child_process')
+var fm = require("fm")
+var cp = require("child_process")
 var run = fm.bind(cp.execSync, cp)
 var said = fm.flow(run, trim)
 
