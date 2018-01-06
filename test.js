bb7fabb605b04869cd2499dab7d5cdbeb9a79b55
Ryan Van Etten <ryanve@gmail.com>
2018-01-06 11:26:50 -0800

    Use double quotes (#2)

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function ok(actual, correct) {
-  if (actual !== correct) throw new Error(actual + ' should be ' + correct)
+  if (actual !== correct) throw new Error(actual + " should be " + correct)
 }
 
-var said = require('./');
-ok(said('npm view fm name'), 'fm');
+var said = require("./");
+ok(said("npm view fm name"), "fm");
